7205fe3931a71d7669d2ffefc180aa34e52156fb
yueheng <yuehengli2012@gmail.com>
2014-01-14 11:55:47 -0800

    Hard2

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -9,6 +9,7 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
  f(n) = C(n,0) + C(n-1,1) + C(n-2,2) + ... + C(ceiling(n/2), floor(n/2))
 */
 
+
 public class Solution {
     public int climbStairs(int n) {
         int a = 1;
