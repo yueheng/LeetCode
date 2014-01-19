e719c79ea02426a994877256c5c6ca8a8cae82b5
yueheng <yuehengli2012@gmail.com>
2014-01-18 20:49:34 -0800

    Hard6

--- Search in Rotated Sorted Array.java
+++ Search in Rotated Sorted Array.java
@@ -8,8 +8,6 @@ You are given a target value to search. If found in the array return its index, 
 You may assume no duplicate exists in the array.
 
 Discuss
-
-
 */
 public class Solution {
     public int search(int[] A, int target) {
