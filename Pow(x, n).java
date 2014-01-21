35ba3210d0e4d861105cc339a6738c2df4a2a12b
yueheng <yuehengli2012@gmail.com>
2014-01-21 14:45:32 -0800

    Hard9

--- Pow(x, n).java
+++ Pow(x, n).java
@@ -1,3 +1,12 @@
 /*
 Implement pow(x, n).
 */
+public class Solution {
+    public double pow(double x, int n) {
+        if(n == 0) return 1.0;
+        double half = pow(x, n/2);
+        if(n%2 == 0) return half * half;
+        else if(n > 0) return half * half * x;
+        else return half * half / x;
+    }
+}
